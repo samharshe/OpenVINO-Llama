70da13faf560ee831fc71c03ea7321927c00fb7e
samharshe <samharshe29@gmail.com>
2025-05-30 12:15:49 -0700

    it has become clear that model surgery in PyTorch is the only way forward. checking in now before rolling up my sleeves.

--- ov_ir/ov_ir.py
+++ ov_ir/ov_ir.py
@@ -6,27 +6,6 @@ model_name = "model_path"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    torch_dtype=torch.float16,  # or torch.float32 if you don't have enough GPU memory
+    torch_dtype=torch.float16,
     device_map="auto"
 )
-
-prompt = "Give me a short introduction to large language model."
-messages = [
-    {"role": "user", "content": prompt}
-]
-text = tokenizer.apply_chat_template(
-    messages,
-    tokenize=False,
-    add_generation_prompt=True,
-    enable_thinking=True
-)
-model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-
-# conduct text completion
-generated_ids = model.generate(
-    **model_inputs,
-    max_new_tokens=32768
-)
-output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist()
-content = tokenizer.decode(output_ids, skip_special_tokens=True).strip("\n")
-print(content)
